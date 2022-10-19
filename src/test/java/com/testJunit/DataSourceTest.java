96dbc074baa56ddef9fd357a5e521a2763810d5f
Sohee Jang <jangsh229@gmail.com>
2022-10-19 14:24:16 +0900

    build: log4j2 maven dependency, log4j2xml 추가

--- src/test/java/com/testJunit/DataSourceTest.java
+++ src/test/java/com/testJunit/DataSourceTest.java
@@ -7,20 +7,25 @@ import javax.sql.DataSource;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.platform.commons.logging.LoggerFactory;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import lombok.extern.log4j.Log4j2;
+
 @RunWith(SpringRunner.class)
 @ContextConfiguration(locations={
 		"file:src/main/webapp/WEB-INF/spring/*.xml"})
+@Log4j2
 public class DataSourceTest {
 	@Autowired
 	private DataSource ds;
-	
 	@Autowired
 	private SqlSessionFactory sf;
+	
 	
 	@DisplayName("dataSource 연결 확인")
 	@Test
@@ -32,5 +37,10 @@ public class DataSourceTest {
 	@Test
 	public void sfTest()throws Exception{
 		assertNotNull(sf);
-	}	
+	}
+	
+	@Test
+	public void log4jTest()throws Exception{
+		log.info("log4j 테스트");
+	}
 }
